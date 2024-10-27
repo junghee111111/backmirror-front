b0d02fef0895a6965fed4a52c999bf76011b4245
junghee111111 <jungheejjang1004@gmail.com>
2024-10-28 02:15:38 +0900

    chore: add description

--- src/app/ui/client/area-visualizer/visualizer/renderer/util.ts
+++ src/app/ui/client/area-visualizer/visualizer/renderer/util.ts
@@ -1,3 +1,11 @@
+/**
+ * @author JungHee Wang (github : junghee111111)
+ * @since 2024-10-28
+ *
+ * @description This file provides utility functions for checking if a file is an image or video file.
+ * It also provides a function to get the file extension.
+ */
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function checkIfImageFile(obj: any) {
   if (typeof obj === "string") {
